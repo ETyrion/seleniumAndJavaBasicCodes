2d51729010e608d57d94c3100cae4b0c92eaabbd
abhishek.mishra@ukg.com <22abhimishra1993@gmail.com>
2021-01-27 00:30:52 +0530

    Calendar code added

--- src/test/java/newPractice/CalendarCode.java
+++ src/test/java/newPractice/CalendarCode.java
@@ -11,26 +11,50 @@ import org.testng.annotations.Test;
 
 public class CalendarCode {
   WebDriver driver;
+  String dateTobeSelected = "15-June";
+  String[] pickDate = dateTobeSelected.split("-");
+
 
   @BeforeTest
   public void launchBrowser() {
     System.setProperty("webdriver.chrome.driver", "E:\\AutomationPractice\\SeleniumPractice\\chromedriver_win32\\chromedriver.exe");
     driver = new ChromeDriver();
     driver.get("https://www.path2usa.com/travel-companions");
+    driver.manage().window().maximize();
   }
 
   @Test(description = "Test to select the desired date from UI")
   public void selectDate() throws InterruptedException {
+    System.out.println(pickDate[0]);
+    System.out.println(pickDate[1]);
     WebElement calendar = driver.findElement(By.id("travel_date"));
+    Thread.sleep(3000);
     calendar.click();
     Thread.sleep(2000);
-    List<WebElement> dates = driver.findElements(By.xpath("//*[@class='day']"));
-    for (int i = 0; i < dates.size(); i++) {
-      Thread.sleep(1000);
-      if (dates.get(i).getText().equalsIgnoreCase("30")) {
-        dates.get(i).click();
-        break;
-      }
+    WebElement arrow = driver.findElement(By.xpath("(//*[@class='next'])[1]"));
+    String month;
+    arrow.click();
+    for(int i=0; i<12; i++)
+    {
+       month= driver.findElement(By.xpath("(//*[@class='datepicker-switch'])[1]")).getText();
+       if(month.contains(pickDate[1]))
+       {
+         List<WebElement> dates = driver.findElements(By.xpath("//*[@class='day']"));
+         for (int j = 0; j < dates.size(); j++) {
+           Thread.sleep(1000);
+           if (dates.get(j).getText().equalsIgnoreCase(pickDate[0])) {
+             dates.get(j).click();
+             break;
+           }
+         }
+         break;
+       }
+       else
+       {
+         arrow.click();
+       }
     }
+
+
   }
 }
